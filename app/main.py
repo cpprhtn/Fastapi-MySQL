12ff393db9483706aa36aaddda42b50bc66c1879
Junwon Lee <cpprhtn@naver.com>
2022-02-26 23:47:45 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,31 @@
 from typing import Optional
 
+import uvicorn
 from fastapi import FastAPI
 
+from app.common.config import conf
 
 
-app = FastAPI()
+def create_app():
+    """
+    앱 함수 실행
+    :return:
+    """
+    c = conf()
+    app = FastAPI()
+
+    # 데이터 베이스 이니셜라이즈
+
+    # 레디스 이니셜라이즈
+
+    # 미들웨어 정의
+
+    # 라우터 정의
+
+    return app
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app = create_app()
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
